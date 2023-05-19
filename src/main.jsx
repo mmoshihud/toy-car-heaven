4f15643695797d372fe0df21887b6de01992b092
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-05-19 20:46:40 +0600

    firebase and auth context created

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './components/provider/AuthProvider'
 import router from './routes/Routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
