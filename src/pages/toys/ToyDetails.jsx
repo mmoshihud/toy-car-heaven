365eca170ec24fcb449b3f2f8337f61dd9a1a4bb
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-05-21 23:59:16 +0600

    project done

--- src/pages/toys/ToyDetails.jsx
+++ src/pages/toys/ToyDetails.jsx
@@ -1,36 +1,28 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { useLoaderData, useParams } from "react-router-dom";
-import Recipe from "../../components/recipe/Recipe";
+import { useParams } from "react-router-dom";
 
 const ToyDetails = () => {
   const { id } = useParams();
-  const toyDetails = useLoaderData();
-  const [recipe, setRecipe] = useState([]);
+  const [toyDetails, setToyDetails] = useState([]);
   useEffect(() => {
-    fetch("https://chef-backend-eta.vercel.app/recipe/" + id)
+    fetch(
+      "https://b7a11-toy-marketplace-server-side-mmoshihud.vercel.app/toys/" +
+        id
+    )
       .then((response) => response.json())
-      .then((data) => setRecipe(data));
+      .then((data) => setToyDetails(data));
   }, [toyDetails]);
   return (
     <>
       <div className="mb-4 flex flex-col items-center justify-center gap-4">
         <img
-          src={toyDetails.imageURL}
+          src={toyDetails.photoURL}
           alt="Chef"
           className="h-80 w-80 rounded-full object-cover"
         />
         <h2 className="text-4xl font-bold">{toyDetails.name}</h2>
-        <p className="w-2/3 text-center text-2xl">{toyDetails.bio}</p>
-        <div className="text-lg font-bold">{toyDetails.likes} likes</div>
-        <div className="text-lg font-bold">
-          {toyDetails.num_recipes} recipes
-        </div>
-        <div className="text-lg font-bold">
-          {toyDetails.experience_years} years of experience
-        </div>
       </div>
-      <Recipe recipes={recipe} />
     </>
   );
 };
