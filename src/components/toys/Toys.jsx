225de41001c654f0496659fedce07159cf3f01dc
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-05-22 22:05:46 +0600

    README updated

--- src/components/toys/Toys.jsx
+++ src/components/toys/Toys.jsx
@@ -1,30 +1,71 @@
 import { Link } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Toys = (props) => {
   return (
-    <div className="container mx-auto grid grid-cols-3 gap-2">
-      {props.toys.map((toy) => (
-        <div key={toy._id} className="rounded-xl shadow-lg">
-          <img
-            className="w-full rounded-xl"
-            src={toy.photoURL}
-            alt="Product Image"
-          />
-          <div className="px-6 py-4">
-            <div className="mb-2 text-xl font-bold">{toy.name}</div>
-            <p className="text-base text-gray-700">${toy.price}</p>
-          </div>
-          <div className="px-6 py-4">
-            <Link
-              to={"/toy/" + toy._id}
-              className="rounded bg-accent px-4 py-2 font-bold text-white hover:bg-accent-focus"
-            >
-              View Details
-            </Link>
-          </div>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="container mx-auto">
+        <Tabs>
+          <TabList className="react-tabs__tab-list">
+            <Tab className="react-tabs__tab">Title 1</Tab>
+            <Tab className="react-tabs__tab">Title 2</Tab>
+            <Tab className="react-tabs__tab">Title 2</Tab>
+          </TabList>
+
+          <TabPanel className="react-tabs__tab-panel">
+            <div className="container mx-auto grid grid-cols-3 gap-2">
+              {props.toys.map((toy) => (
+                <div key={toy._id} className="rounded-xl shadow-lg">
+                  <img
+                    className="w-full rounded-xl"
+                    src={toy.photoURL}
+                    alt="Product Image"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="mb-2 text-xl font-bold">{toy.name}</div>
+                    <p className="text-base text-gray-700">${toy.price}</p>
+                  </div>
+                  <div className="px-6 py-4">
+                    <Link
+                      to={"/toy/" + toy._id}
+                      className="rounded bg-accent px-4 py-2 font-bold text-white hover:bg-accent-focus"
+                    >
+                      View Details
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="grid grid-cols-3 gap-2">
+              {props.toys.map((toy) => (
+                <div key={toy._id} className="rounded-xl shadow-lg">
+                  <img
+                    className="w-full rounded-xl"
+                    src={toy.photoURL}
+                    alt="Product Image"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="mb-2 text-xl font-bold">{toy.name}</div>
+                    <p className="text-base text-gray-700">${toy.price}</p>
+                  </div>
+                  <div className="px-6 py-4">
+                    <Link
+                      to={"/toy/" + toy._id}
+                      className="rounded bg-accent px-4 py-2 font-bold text-white hover:bg-accent-focus"
+                    >
+                      View Details
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </TabPanel>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
